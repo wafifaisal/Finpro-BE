9718a4f40433efab137c5d6279574c838030734e
Wafi Faisal Falah <wafifaisal19@gmail.com>
2025-02-04 19:43:52 +0700

    User Profile Done

---
+++ src/router/user.router.ts
@@ -0,0 +1,54 @@
+import { Router } from "express";
+import { verifyTokenUser } from "../middleware/verify.user";
+import { uploader } from "../services/uploader";
+import { UserController } from "../controller/feature1/user/user.controller";
+
+export class UserRouter {
+  private userController: UserController;
+  private router: Router;
+
+  constructor() {
+    this.userController = new UserController();
+    this.router = Router();
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get("/", this.userController.getUser);
+    this.router.patch(
+      "/update-email",
+      verifyTokenUser,
+      this.userController.updateEmail
+    );
+    this.router.patch(
+      "/resend-verification/:token",
+      verifyTokenUser,
+      this.userController.verifyEmail
+    );
+
+    this.router.patch(
+      "/update-password",
+      verifyTokenUser,
+      this.userController.updatePassword
+    );
+
+    this.router.patch(
+      "/avatar-cloud",
+      verifyTokenUser,
+      uploader("memoryStorage", "avatar").single("file"),
+      this.userController.editAvatarUser
+    );
+
+    this.router.get(
+      "/profile",
+      verifyTokenUser,
+      this.userController.getUserProfile
+    );
+
+    this.router.delete("/:id", this.userController.deleteUser);
+  }
+
+  getRouter(): Router {
+    return this.router;
+  }
+}
